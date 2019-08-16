b441216cf75df8bf9980f4d51e4e847a4df8a43f
jcespitiam <53576217+jcespitiam@users.noreply.github.com>
2019-08-16 13:34:58 -0500

    Update Palindromo.java

--- Palindromo.java
+++ Palindromo.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Aprendiz
+ * @author JuanCamilo
  */
 public class Palindromo {
 
@@ -18,13 +18,13 @@ public class Palindromo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String palabra;
-        palabra=JOptionPane.showInputDialog(null, "ingrese la palabra");
-        palabra = palabra.toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
+        String textIn;
+        textIn=JOptionPane.showInputDialog(null, "ingrese la palabra");
+        textIn = textIn.toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
                 .replace("ú", "u").replace(" ", "").replace(".", "").replace(",", "");
-        String invertida = new StringBuilder(palabra).reverse().toString();
+        String reverseText = new StringBuilder(textIn).reverse().toString();
         
-        if (invertida.equals(palabra)){
+        if (invertida.equals(textIn)){
             JOptionPane.showMessageDialog(null, "es palindromo");
         }else{
             JOptionPane.showMessageDialog(null, "no es palindromo");
